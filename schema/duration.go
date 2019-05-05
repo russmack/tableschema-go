e6b9f36758bea40db7b615da90748f5bd300c549
Russell Mackenzie <russmackdev@gmail.com>
2019-05-05 01:44:54 +0100

    Add ISODate duration parser.

--- schema/duration.go
+++ schema/duration.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"reflect"
 	"regexp"
@@ -13,12 +15,136 @@ var durationRegexp = regexp.MustCompile(
 	`P(?P<years>\d+Y)?(?P<months>\d+M)?(?P<days>\d+D)?T?(?P<hours>\d+H)?(?P<minutes>\d+M)?(?P<seconds>\d+\.?\d*S)?`)
 
 const (
-	hoursInYear  = time.Duration(24*360) * time.Hour
-	hoursInMonth = time.Duration(24*30) * time.Hour
-	hoursInDay   = time.Duration(24) * time.Hour
+	hoursInYear   = time.Duration(24*365) * time.Hour
+	hoursInMonth  = time.Duration(24*30) * time.Hour
+	hoursInWeek   = time.Duration(24*7) * time.Hour
+	hoursInDay    = time.Duration(24) * time.Hour
+	minutesInHour = time.Duration(60) * time.Minute
+
+	yearNanos   = 365.25 * 24 * float64(time.Hour)    // 31557600000000000
+	monthNanos  = 30.4375 * 24.0 * float64(time.Hour) // 2629800000000000
+	weekNanos   = 7 * 24.0 * float64(time.Hour)       // 604800000000000
+	dayNanos    = 24.0 * float64(time.Hour)           // 86400000000000
+	hourNanos   = 1 * float64(time.Hour)              // 3600000000000
+	minuteNanos = 1 * float64(time.Minute)            // 60000000000
+	secondNanos = 1 * float64(time.Second)            // 1000000000
+
+	seps          = "PYMWDTHMS"
+	indexOfMonths = 2
+	indexOfT      = 5
+	delimTime     = 'T'
+	unitM         = 'M' // Ambiguous Months and Minutes
+	prefix        = 'P'
 )
 
+func parseISODateDuration(dur string) ([7]float64, error) {
+	// Example: d := "P3Y6M4DT12H30M5S"
+
+	// Matches store.
+	m := [7]float64{}
+	if dur == "" {
+		return m, errors.New("error: empty")
+	}
+	if len(dur) <= 2 {
+		return m, errors.New("error: duration is too short to be valid")
+	}
+	// Ensure duration starts with 'P'.
+	if dur[0] != seps[0] {
+		return m, errors.New("error: missing 'P' prefix")
+	}
+	// Index pointers for duration and separator strings, and matches slice.
+	di := 1
+	si := 1
+	mi := 0
+	b := bytes.NewBuffer(make([]byte, 0, 64))
+	// Loop over duration, collecting number then character, repeatedly.
+	for si < len(seps) {
+		if di >= len(dur) {
+			break
+		}
+		// Consume numeric.
+		for dur[di] >= '0' && dur[di] <= '9' {
+			b.WriteByte(dur[di])
+			di++
+		}
+		if dur[di] == '.' {
+			b.WriteByte(dur[di])
+			di++
+			if dur[di] >= '0' && dur[di] <= '9' {
+				for dur[di] >= '0' && dur[di] <= '9' {
+					b.WriteByte(dur[di])
+					di++
+				}
+			} else {
+				return m, errors.New("error: missing digit after decimal")
+			}
+		}
+		// Consume letter.
+		// Iterate over separators (si),
+		// looking for a match for the current duration character (di).
+		for si < len(seps) {
+			if dur[di] == seps[si] {
+				// If unit is T skip.
+				if dur[di] != delimTime {
+					// Distinguish between 'M' months and 'M' minutes.
+					if dur[di] == unitM && si > indexOfMonths {
+						mi = indexOfT
+					}
+					f, err := strconv.ParseFloat(b.String(), 64)
+					if err != nil {
+						return m, err
+					}
+					m[mi] = f
+				}
+				b.Reset()
+				di++
+				si++
+				if di < len(dur) && dur[di] != delimTime {
+					mi++
+				}
+				// Matches - break to store next number.
+				break
+			}
+			// Not a match.
+			if si == len(seps)-1 {
+				return m, errors.New("error: letter not a valid unit")
+			}
+			si++
+			if seps[si] == delimTime {
+				continue
+			}
+			mi++
+		}
+	}
+	if di < len(dur)-1 {
+		return m, errors.New("error: invalid ISODate")
+	}
+	return m, nil
+}
+
+func floatToDuration(i, nanos float64) time.Duration {
+	var f float64 = i * nanos
+	var n int64 = int64(f)
+	return time.Duration(n)
+}
+
 func castDuration(value string) (time.Duration, error) {
+	matches, err := parseISODateDuration(value)
+	if err != nil {
+		return 0, err
+	}
+	years := floatToDuration(matches[0], yearNanos)
+	months := floatToDuration(matches[1], monthNanos)
+	weeks := floatToDuration(matches[2], weekNanos)
+	days := floatToDuration(matches[3], dayNanos)
+	hours := floatToDuration(matches[4], hourNanos)
+	minutes := floatToDuration(matches[5], minuteNanos)
+	seconds := floatToDuration(matches[6], secondNanos)
+
+	return years + months + days + weeks + hours + minutes + seconds, nil
+}
+
+func castDurationRegex(value string) (time.Duration, error) {
 	matches := durationRegexp.FindStringSubmatch(value)
 	if len(matches) == 0 {
 		return 0, fmt.Errorf("Invalid duration:\"%s\"", value)
--- schema/duration_test.go
+++ schema/duration_test.go
@@ -7,23 +7,136 @@ import (
 	"github.com/matryer/is"
 )
 
-func TestCastDuration_Success(t *testing.T) {
-	data := []struct {
+var (
+	castDurationSuccessData = []struct {
 		desc  string
 		value string
 		want  time.Duration
 	}{
-		{"OnlyP", "P", time.Duration(0)},
-		{"OnlyHour", "P2H", time.Duration(2) * time.Hour},
-		{"SecondsWithDecimal", "P22.519S", 22519 * time.Millisecond},
-		{"HourDefaultZero", "PH", time.Duration(0) * time.Hour},
-		{"OnlyPeriod", "P3Y6M4D", 3*hoursInYear + 6*hoursInMonth + 4*hoursInDay},
-		{"OnlyTime", "PT12H30M5S", 12*time.Hour + 30*time.Minute + 5*time.Second},
-		{"Complex", "P3Y6M4DT12H30M5S", 3*hoursInYear + 6*hoursInMonth + 4*hoursInDay + 12*time.Hour + 30*time.Minute + 5*time.Second},
-		{"2Years", "P2Y", (2 * 360 * 24) * time.Hour},
-		{"StringFieldsAreIgnored", "PfooHdddS", time.Duration(0)},
+		{"OnlyHour", "P2H", floatToDuration(2, hourNanos)},
+		{"2Years", "P2Y", floatToDuration(2, yearNanos)},
+		{"SecondsWithDecimal", "P22.519S", floatToDuration(22.519, secondNanos)},
+		{"Month Duration", "P1M", floatToDuration(1, monthNanos)},
+		{"Week Duration", "P1W", floatToDuration(1, weekNanos)},
+		{"Day Duration", "P1D", floatToDuration(1, dayNanos)},
+		{"Week Duration with fraction", "P1.5W", floatToDuration(1.5, weekNanos)},
+		{"OnlyPeriod", "P3Y6M4D",
+			floatToDuration(3, yearNanos) +
+				floatToDuration(6, monthNanos) +
+				floatToDuration(4, dayNanos)},
+		{"OnlyTime", "PT12H30M5S",
+			floatToDuration(12, hourNanos) +
+				floatToDuration(30, minuteNanos) +
+				floatToDuration(5, secondNanos)},
+		{"Complex", "P3Y6M4DT12H30M5S",
+			floatToDuration(3, yearNanos) +
+				floatToDuration(6, monthNanos) +
+				floatToDuration(4, dayNanos) +
+				floatToDuration(12, hourNanos) +
+				floatToDuration(30, minuteNanos) +
+				floatToDuration(5, secondNanos)},
+		{"Duration", "P1Y2M10DT2H30M",
+			floatToDuration(1, yearNanos) +
+				floatToDuration(2, monthNanos) +
+				floatToDuration(10, dayNanos) +
+				floatToDuration(2, hourNanos) +
+				floatToDuration(30, minuteNanos)},
+		{"Duration with fraction", "P5.1Y3.5M3.4W2.5D",
+			floatToDuration(5.1, yearNanos) +
+				floatToDuration(3.5, monthNanos) +
+				floatToDuration(3.4, weekNanos) +
+				floatToDuration(2.5, dayNanos)},
+		{"Duration with time with fractions", "P1.5Y2.5M10.5DT2.5H30.5M",
+			floatToDuration(1.5, yearNanos) +
+				floatToDuration(2.5, monthNanos) +
+				floatToDuration(10.5, dayNanos) +
+				floatToDuration(2.5, hourNanos) +
+				floatToDuration(30.5, minuteNanos)},
 	}
-	for _, d := range data {
+
+	castDurationErrorData = []struct {
+		desc  string
+		value string
+	}{
+		{"WrongStartChar", "C2H"},
+		{"HourDefaultZero", "PH"},
+		{"StringFieldsAreIgnored", "PfooHdddS"},
+		{"String NULL", "NULL"},
+		{"String NULL_TYPE", "NULL_TYPE"},
+		{"String NOT_ALLOWED", "NOT_ALLOWED"},
+		{"String NOT_AVAILABLE", "NOT_AVAILABLE"},
+		{"String TEMPLATE", "TEMPLATE"},
+		{"String TYPE", "TYPE"},
+		{"String NO_MATCHING_TYPE", "NO_MATCHING_TYPE"},
+		{"Empty P", "P"},
+		{"Double P", "PP"},
+		{"P with no units", "P1"},
+		{"P with unit P", "P1P"},
+		{"P with numeric prefix", "1P"},
+	}
+)
+
+func parseDuration(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(err)
+	}
+	return d
+}
+
+func BenchmarkCastDuration_Success(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, d := range castDurationSuccessData {
+			res, _ := castDuration(d.value)
+			_ = res
+		}
+	}
+}
+
+func BenchmarkCastDuration_Error(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, d := range castDurationErrorData {
+			res, _ := castDuration(d.value)
+			_ = res
+		}
+	}
+}
+
+/* */
+func BenchmarkCastDurationRegex_Success(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, d := range castDurationSuccessData {
+			res, _ := castDurationRegex(d.value)
+			_ = res
+		}
+	}
+}
+
+func BenchmarkCastDurationRegex_Error(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, d := range castDurationErrorData {
+			res, _ := castDurationRegex(d.value)
+			_ = res
+		}
+	}
+}
+
+/* */
+
+func TestCastDuration_Success(t *testing.T) {
+	for _, d := range castDurationSuccessData {
 		t.Run(d.desc, func(t *testing.T) {
 			is := is.New(t)
 			got, err := castDuration(d.value)
@@ -34,13 +147,7 @@ func TestCastDuration_Success(t *testing.T) {
 }
 
 func TestCastDuration_Error(t *testing.T) {
-	data := []struct {
-		desc  string
-		value string
-	}{
-		{"WrongStartChar", "C2H"},
-	}
-	for _, d := range data {
+	for _, d := range castDurationErrorData {
 		t.Run(d.desc, func(t *testing.T) {
 			is := is.New(t)
 			_, err := castDuration(d.value)
@@ -48,6 +155,30 @@ func TestCastDuration_Error(t *testing.T) {
 		})
 	}
 }
+
+/* */
+func TestCastDurationRegex_Success(t *testing.T) {
+	for _, d := range castDurationSuccessData {
+		t.Run(d.desc, func(t *testing.T) {
+			is := is.New(t)
+			got, err := castDurationRegex(d.value)
+			is.NoErr(err)
+			is.Equal(got, d.want)
+		})
+	}
+}
+
+func TestCastDurationRegex_Error(t *testing.T) {
+	for _, d := range castDurationErrorData {
+		t.Run(d.desc, func(t *testing.T) {
+			is := is.New(t)
+			_, err := castDurationRegex(d.value)
+			is.True(err != nil)
+		})
+	}
+}
+
+/* */
 
 func TestUncastDuration(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
